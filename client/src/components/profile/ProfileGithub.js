e2706ee0a1c5a4b4b13198f93cb974487970807b
Bojan Mitic <bojanmitic84@yahoo.com>
2018-12-02 21:18:47 +0200

    github links updated

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 class ProfileGithub extends Component {
@@ -37,9 +36,7 @@ class ProfileGithub extends Component {
         <div className="row">
           <div className="col-md-6">
             <h4>
-              <Link to={repo.html_url} className="text-info" target="_blank">
-                {repo.name}
-              </Link>
+              <a href={repo.html_url} className="text-info" target="_blank">{repo.name}</a>
             </h4>
             <p>{repo.description}</p>
           </div>
